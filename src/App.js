8df074a608173f887d3856e8c4e92064af246417
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2022-10-10 16:15:56 +0530

    feat(setup): init setup with crayons library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { FwButton } from "@freshworks/crayons/react";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FwButton onClick={() => alert("button clicked")}>Button</FwButton>
     </div>
   );
 }
